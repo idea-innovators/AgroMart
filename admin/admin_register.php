381958dc502e82d20a5a27f88e4ca01f0f3a35de
sumudu-k <lakshankulathunga21@gmail.com>
2024-12-07 20:25:24 +0530

    creates admin registration form page

---
+++ admin/admin_register.php
@@ -0,0 +1,7 @@
+<form method="POST" action="admin_register.php">
+    <input type="text" name="username" placeholder="Admin Username" required><br>
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="email" name="confirm_email" placeholder="Confirm Email" required><br>
+    <input type="password" name="password" placeholder="Password" required><br>
+    <button type="submit">Register</button>
+</form>
